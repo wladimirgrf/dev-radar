948fd8cc750a58dfd8f194a3f94ee14a3bac4add
Wladimir <wladimirgrf@gmail.com>
2020-01-19 18:43:13 -0300

    web - change baseURL for gitpod domain

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: 'https://3333-ba759df8-60eb-4baa-8eef-738f55487a19.ws-us02.gitpod.io'
 });
 
 export default api
