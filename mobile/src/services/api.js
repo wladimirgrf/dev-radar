3274ad98c5f67c98c382ab6de714349530581661
Wladimir <wladimirgrf@gmail.com>
2020-04-06 22:19:08 -0300

    update: API & Socket baseURL, mongodb Connection URL

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
+//Replace Your_IP with your IP (192.168.?.?)
 
 const api = axios.create({
-    baseURL:'https://3333-a42b1b0f-155b-485d-88a0-6fc04109c8e2.ws-us02.gitpod.io',
+    baseURL: `http://${Your_IP}:3333`,
 })
 
 export default api
